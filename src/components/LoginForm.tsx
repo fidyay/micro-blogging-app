1151f2403c9db1ea3ae9899bea98614f04de9361
Fidyay <fidiy1878@gmail.com>
2023-09-01 19:30:25 +0300

    login form finished

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,85 @@
+import AppBlockWrapper from "./AppBlockWrapper";
+import Heading from "./Heading";
+import TextButton from "./TextButton";
+import AppTextField from "./AppTextField";
+import { useRouter } from "next/router";
+import { Typography } from "@mui/material";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useMutation } from "react-query";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+
+function useLoginMutation() {
+  const router = useRouter();
+  const supabaseClient = useSupabaseClient();
+  async function Login(userData: Inputs) {
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
+      email: userData.email,
+      password: userData.password,
+    });
+  }
+  return useMutation(async (userData: Inputs) => Login(userData), {
+    onError: console.error,
+    onSuccess() {
+      router.push("/general");
+    },
+  });
+}
+
+interface Inputs {
+  email: string;
+  password: string;
+}
+
+function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const loginMutation = useLoginMutation();
+
+  return (
+    <AppBlockWrapper
+      sx={{
+        width: 300,
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        padding: 1,
+      }}
+    >
+      <form
+        onSubmit={handleSubmit(loginMutation.mutate as SubmitHandler<Inputs>)}
+        className="form_sign-in"
+      >
+        <Heading sx={{ mb: 1 }}>Login</Heading>
+        {Object.keys(errors).length ? (
+          <Typography sx={{ color: "warning.main" }}>
+            Some error occured :(
+          </Typography>
+        ) : null}
+        <AppTextField
+          sx={{
+            my: 1,
+          }}
+          required
+          label="Email"
+          type="email"
+          register={register("email")}
+        />
+        <AppTextField
+          type="password"
+          sx={{ my: 1 }}
+          required
+          label="Password"
+          register={register("password")}
+        />
+        <TextButton type="submit">Submit</TextButton>
+      </form>
+    </AppBlockWrapper>
+  );
+}
+
+export default LoginForm;
