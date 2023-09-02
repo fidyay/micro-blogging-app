e4d5832122fd37d1f93ac5aa544a90707069b494
Fidyay <fidiy1878@gmail.com>
2023-09-02 14:02:54 +0300

    post creation form is working

--- src/components/PostCreationForm.tsx
+++ src/components/PostCreationForm.tsx
@@ -1,21 +1,84 @@
 import AppBlockWrapper from "./AppBlockWrapper";
-import { useState } from "react";
 import Heading from "./Heading";
 import TextButton from "./TextButton";
 import AppTextField from "./AppTextField";
 import AppImagePicker from "./AppImagePicker";
+import {
+  useForm,
+  Controller,
+  SubmitHandler,
+  UseFormReset,
+} from "react-hook-form";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { useMutation, useQueryClient } from "react-query";
+import ErrorText from "./ErrorText";
+
+interface Inputs {
+  title: string;
+  description: string;
+  img: File | null;
+}
+
+function useCreatePostMutation(resetForm: UseFormReset<Inputs>) {
+  const queryClient = useQueryClient();
+  const user = useUser();
+  const supabaseClient = useSupabaseClient();
+  async function createPost(postData: Inputs) {
+    const { data: postDbData, error: postError } = await supabaseClient
+      .from("posts")
+      .insert([
+        {
+          title: postData.title,
+          description: postData.description,
+          has_image: !!postData.img,
+          author_id: user?.id as string,
+        },
+      ])
+      .select();
+    if (postError) console.error(postError);
+    if (postDbData && postData.img) {
+      const savedPost = postDbData[0];
+      const { data, error } = await supabaseClient.storage
+        .from("images")
+        .upload(`post_pictures/${savedPost.id}`, postData.img, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+    }
+  }
+  return useMutation(async (postData: Inputs) => createPost(postData), {
+    onError: console.error,
+    onSuccess() {
+      resetForm();
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
+}
 
 function FormCreationForm() {
-  const [value, setValue] = useState<File | null>(null);
+  const {
+    register,
+    control,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      img: null,
+    },
+  });
 
-  const handleChange = (newValue: File | null) => {
-    setValue(newValue);
-  };
+  const createPostMutation = useCreatePostMutation(reset);
 
   return (
     <AppBlockWrapper sx={{ mb: 1, p: 1 }}>
-      <form>
+      <form
+        onSubmit={handleSubmit(
+          createPostMutation.mutate as SubmitHandler<Inputs>
+        )}
+      >
         <Heading>Create post</Heading>
+        {Object.keys(errors).length ? <ErrorText /> : null}
         <AppTextField
           sx={{
             my: 1,
@@ -23,6 +86,7 @@ function FormCreationForm() {
           required
           label="Title"
           multiline
+          register={register("title")}
         />
         <AppTextField
           sx={{ my: 1 }}
@@ -30,14 +94,26 @@ function FormCreationForm() {
           label="Description"
           multiline
           minRows={3}
+          register={register("description")}
         />
-        <AppImagePicker
-          sx={{ my: 1 }}
-          label="Select image"
-          value={value}
-          onChange={handleChange}
+        <Controller
+          name="img"
+          control={control}
+          render={({ field, fieldState }) => {
+            return (
+              <AppImagePicker
+                {...field}
+                helperText={fieldState.invalid ? "File is invalid" : ""}
+                error={fieldState.invalid}
+                sx={{ my: 1 }}
+                label="Select image"
+              />
+            );
+          }}
         />
-        <TextButton>Submit</TextButton>
+        <TextButton disabled={createPostMutation.isLoading} type="submit">
+          Submit
+        </TextButton>
       </form>
     </AppBlockWrapper>
   );
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,11 +1,31 @@
 import { Box } from "@mui/material";
 import Post from "./Post";
 import PostCreationForm from "./PostCreationForm";
+import { useUserInfoQuery } from "./UserAccountControls";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import ErrorText from "./ErrorText";
+import AppBlockWrapper from "./AppBlockWrapper";
 
 function PostList() {
+  const router = useRouter();
+  const user = useUser();
+  const supabase = useSupabaseClient();
+  const { data: userInfoData, isError: userInfoError } = useUserInfoQuery(
+    user?.id as string,
+    supabase
+  );
+  const shouldPostCreationFormRender = !!(
+    router.pathname.includes("general") && userInfoData?.is_author
+  );
   return (
     <Box>
-      <PostCreationForm />
+      {userInfoError ? (
+        <AppBlockWrapper sx={{ mb: 1, p: 1 }}>
+          <ErrorText />
+        </AppBlockWrapper>
+      ) : null}
+      {shouldPostCreationFormRender ? <PostCreationForm /> : null}
       {[1, 2, 3].map((item, index) => (
         <Post id={String(item)} key={item} />
       ))}
